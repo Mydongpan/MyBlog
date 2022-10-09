22b6ba139bcfb74d053fc4691cb9accc4a84c2d7
Myuan <1209619967@qq.com>
2022-10-09 22:30:35 +0800

    two commit

---
+++ manong-blog/src/main/java/com/manong/MaNongBlogApplication.java
@@ -0,0 +1,11 @@
+package com.manong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaNongBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MaNongBlogApplication.class,args);
+    }
+}
---
+++ manong-framework/src/main/java/com/manong/dpmain/entity/Article.java
@@ -0,0 +1,55 @@
+package com.manong.dpmain.entity;
+
+import java.util.Date;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * 文章表(Article)表实体类
+ *
+ * @author makejava
+ * @since 2022-10-09 22:24:49
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Article {
+    
+    private Long id;
+    //标题
+    private String title;
+    //文章内容
+    private String content;
+    //文章摘要
+    private String summary;
+    //所属分类id
+    private Long categoryId;
+    //缩略图
+    private String thumbnail;
+    //是否置顶（0否，1是）
+    private String isTop;
+    //状态（0已发布，1草稿）
+    private String status;
+    //访问量
+    private Long viewCount;
+    //是否允许评论 1是，0否
+    private String isComment;
+    
+    private Long createBy;
+    
+    private Date createTime;
+    
+    private Long updateBy;
+    
+    private Date updateTime;
+    //删除标志（0代表未删除，1代表已删除）
+    private Integer delFlag;
+
+    }
+
